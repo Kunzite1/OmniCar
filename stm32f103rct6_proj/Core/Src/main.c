a2a71fff9cef5f76a6257144f36c60a3e61cc4de
Kunzite <wang_zhandong@outlook.com>
2026-09-21 12:59:53 +0800

    feat: 配置好IMU的硬件I2C引脚

--- stm32f103rct6_proj/Core/Src/main.c
+++ stm32f103rct6_proj/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "can.h"
+#include "i2c.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -94,6 +95,7 @@ int main(void)
   MX_USART3_UART_Init();
   MX_TIM3_Init();
   MX_CAN_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
